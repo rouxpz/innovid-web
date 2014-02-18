d93d35ab16e5a686d8c192fbfd8418482a45033d
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2014-02-18 18:59:50 -0500

    added models, requirements

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-import os
+import os, re
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask.ext.mongoengine import mongoengine
 from werkzeug import secure_filename
+import boto
+import models
 
 
 #path to upload directory
 UPLOAD_FOLDER = 'uploads'
 
 #allowed extensions
-ALLOWED_EXTENSIONS = set(['pdf', 'jpg', 'jpeg', 'png', 'mov'])
+ALLOWED_EXTENSIONS = set(['mov'])
 
 #initialize Flask application
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+mongoengine.connect('mydata', host=os.environ.get('MONGOLAB_URI'))
 
 names = []
 
@@ -21,22 +26,49 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
-	if request.method == 'POST':
-		file = request.files['file']
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			names.append(str(filename))
-			return redirect(url_for('uploaded_file', filename=filename))
+def index():
 
-	return render_template('index.html')
+	video_upload_form = models.video_upload_form(request.form)
+
+	if request.method == 'POST' and video_upload_form.validate():
+
+		#create filename
+		uploaded_file = request.files['fileupload']
+		if uploaded_file and allowed_file(uploaded_file.filename):
+			filename = secure_filename(uploaded_file.filename)
+
+			#connecting to s3
+			s3 = boto.connect_s3(os.environ.get('AWS_ACCESS_KEY_ID'), os.environ.get('AWS_SECRET_ACCESS_KEY'))
+			bucket = s3.get_bucket(os.environ.get('AWS_BUCKET'))
+
+			k = bucket.new_key(bucket)
+			k.key = filename #set filename
+			k.set_metadata("Content Type", uploaded_file.mimetype) #identify MIME type
+			k.set_contents_from_string(uploaded_file.stream.read()) # file contents to be added
+			k.set_acl('public-read') # make publicly readable
+
+
+			#if the file was actually uploaded:
+			if k and k.size > 0:
+
+				submission = models.Video()
+				submission_title = request.form.get('title')
+				submission.save()
+
+				return redirect('/')
+
+		else:
+
+			return "Uh-oh, there was an error" + uploaded_file.filename
+	else:
+
+		return render_template('index.html')
 
 
 #expecting the name of a file -- will locate file on the upload directory and show in the browser
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
+# 	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
 	app.run()
---
+++ models.py
@@ -0,0 +1,16 @@
+from flask.ext.mongoengine.wtf import model_form
+from wtforms.fields import * # for our custom signup form
+from flask.ext.mongoengine.wtf.orm import validators
+from flask.ext.mongoengine import *
+from datetime import datetime
+
+class Video(mongoengine.Document):
+
+	#title of Video
+	title = mongoengine.StringField(max_length=120, required=True)
+	filename = mongoengine.StringField()
+
+video_form = model_form(Video)
+
+class video_upload_form(video_form):
+	fileupload = FileField('Upload a video', validators=[])
