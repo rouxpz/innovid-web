697e106f1ccdad43f5221596767965b3f3717454
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2014-03-01 18:26:43 -0500

    turned debug mode off

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def allowed_file(filename):
 # 	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-	app.debug = os.environ.get('DEBUG', True)
+	app.debug = os.environ.get('DEBUG', False)
 
 	port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port)
